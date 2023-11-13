10e65c47eacf4c5cc45d1f14503f159450beb70f
abhishek <125186953+abhishek887229@users.noreply.github.com>
2023-11-13 09:04:36 +0000

    create a html name prodct detail

--- onshop/shop/views.py
+++ onshop/shop/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Products
 
-# Create your views here.
+
+def show_product(request):
+
+    x=Products.objects.all()
+    return render(request,'shop/Product_detail.html',{'x':x})
+
