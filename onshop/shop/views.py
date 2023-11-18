cda55e88dc58f58a3d99b51e8279c9f38f9bd536
abhishek <125186953+abhishek887229@users.noreply.github.com>
2023-11-18 16:19:31 +0000

    MAKE CHNAGE IN VIEW

--- onshop/shop/views.py
+++ onshop/shop/views.py
@@ -12,11 +12,6 @@ def show_product(request):
     else:
         product_object = x
 
-    # Now, it shows on which object or list we have to apply it and how many products to show at a time.
-    paginator = Paginator(product_object, 4)
-    page = request.GET.get('page')
-    product_object = paginator.get_page(page)
-
     return render(request, 'shop/Product_detail.html', {'x': product_object})
 
 
